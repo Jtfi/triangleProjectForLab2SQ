6cac78d28075ceeeb9fdac940b8bd055d1553c0e
Jtfink <68623825+Jtfi@users.noreply.github.com>
2021-09-17 16:48:50 -0400

    Initial Commit, this shouldn't be included in the actual commits just figuring out how to work Git

---
+++ triangleCode/Triangle.h
@@ -0,0 +1,39 @@
+#pragma once
+/* -- this is a FILEHEADER COMMENT --
+	NAME	:	AmFmRadio
+	PURPOSE :	This has all the information on the AmFmRadio
+				class, it includes all the data members, constants,
+				and methods used in this class, it is a parent of the
+				PioneerCarRadio class
+
+*/
+
+
+
+class Triangle
+{
+	/* ====================================== */
+	/*              PRIVATE                   */
+	/* ====================================== */
+private:
+
+	int sideA;		//One side of the triangle
+	int sideB;		//One side of the triangle
+	int sideC;		//One side of the triangle
+
+	double angleAB;	//Angle with sides A and B
+	double angleBC;	//Angle with sides B and C
+	double angleCA; //Angle with sides C and A
+
+
+	/* ====================================== */
+	/*              PUBLIC                    */
+	/* ====================================== */
+public:
+
+
+
+};
+
+
+
---
+++ triangleCode/main.cpp
@@ -0,0 +1,24 @@
+#pragma once
+/* -- this is a FILEHEADER COMMENT --
+	NAME	:	main
+	PURPOSE :	
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <new>
+
+#include <exception>
+
+#include "Triangle.h"
+
+
+int main()
+{
+
+
+
+}
